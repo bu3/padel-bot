75ceb74e5d4c775b53b76814daf0378a8f915046
Fabio Mangione <fmangione@salesforce.com>
2022-09-03 15:08:47 +0100

    Refactor

--- cypress/e2e/padel.cy.js
+++ cypress/e2e/padel.cy.js
@@ -1,17 +1,16 @@
-describe('empty spec', () => {
-  it('passes', () => {
+describe('Book a court', () => {
+  
+  it('Login', () => {
     cy.visit('http://www.bushytennispadel.ie/')
     cy.get(".BotonAccesoUsuario").click()
     
     const username = Cypress.env('PADEL_USER')
     const pwd = Cypress.env('PADEL_PWD')
 
-    // expect(username).to.not.equal('undefined')
-    // expect(pwd).to.not.equal('undefined')
+    // check for envs to exist
     
     cy.get("input#ctl00_ContentPlaceHolderContenido_Login1_UserName").type(username)
     cy.get("input#ctl00_ContentPlaceHolderContenido_Login1_Password").type(pwd)
     cy.get("input[type=submit]").click()
-
   })
 })
